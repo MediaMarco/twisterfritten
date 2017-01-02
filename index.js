40b02759d324739e614a8c9535165b829ac72c53
mb/mg <ft3-pairing@otto.de>
2017-01-02 11:24:34 +0100

    dap|es6 update

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-var express = require('express');
-var request = require("request");
-var app = express();
+const express = require('express');
+const request = require("request");
+const app = express();
 
-var port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
-var kochwerkUrls = {
-    alster : "http://speiseplan.kochwerk-hamburg.de/home/kochwerk-alster.html",
-    elbe : "http://speiseplan.kochwerk-hamburg.de/home/kochwerk-elbe.html",
-    bonprix : "http://speiseplan.kochwerk-hamburg.de/home/bon-prix.html"
+const kochwerkUrls = {
+    alster: "http://speiseplan.kochwerk-hamburg.de/home/kochwerk-alster.html",
+    elbe: "http://speiseplan.kochwerk-hamburg.de/home/kochwerk-elbe.html",
+    bonprix: "http://speiseplan.kochwerk-hamburg.de/home/bon-prix.html"
 };
 
-var headline = "<h3>Gibt's heute Twisterfritten?</h3>\n";
-var twisterRegex = /twister/i;
+const headline = "<h3>Gibt's heute Twisterfritten?</h3>\n";
+const twisterRegex = /twister/i;
 
 app.use(express.static('public'));
 app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
 
-    var responseCounter = 0;
-    var where = "";
-    var globalMatch = false;
+    let responseCounter = 0;
+    let where = "";
+    let globalMatch = false;
 
-    for (var key in kochwerkUrls) {
+    for (let key in kochwerkUrls) {
         doRequest(key);
     }
 
@@ -33,22 +33,22 @@ app.get('/', function (req, res) {
             } else {
                 res.error(error);
             }
-        }
+        };
         request(kochwerkUrls[key], callback);
     }
 
     function collectResponses(url, res, body) {
         responseCounter++;
-        var match = !!(body.match(twisterRegex));
+        const match = !!(body.match(twisterRegex));
         if (match) {
-            where = where + ("<a href=\"" + url + "\">" + url + "</a><br />");
+            where = `${where}<a href="${url}">${url}</a><br />`;
         }
         globalMatch = globalMatch || match;
         if (responseCounter == Object.keys(kochwerkUrls).length) {
-            var frittenResponse = !!globalMatch ? "<h1>JA</h1>" : "<h1>NEIN</h1>\n";
+            const frittenResponse = globalMatch ? "<h1>JA</h1>" : "<h1>NEIN</h1>\n";
 
             if (globalMatch) {
-                where = "<h4>Wo?<br />" + where + "</h4>";
+                where = `<h4>Wo?<br />${where}</h4>`;
             }
 
             res.render('index', {title: 'Gibt\'s heute Twisterfritten?', message: headline + frittenResponse + where});
